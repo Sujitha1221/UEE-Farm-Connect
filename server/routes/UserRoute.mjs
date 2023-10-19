095222f2b7f9889104159b80cd51bc5097b50db3
Sujitha Srikanthan <sujikanthan2001@gmail.com>
2023-10-19 19:03:06 +0530

    user backend added

---
+++ server/routes/UserRoute.mjs
@@ -0,0 +1,17 @@
+import {
+  createUser,
+  loginUser,
+  changePassword,
+  updateUser,
+  deleteUser,
+} from "../controllers/UserController.mjs";
+import Router from "express";
+
+const UserRouter = Router();
+UserRouter.post("/add-user", createUser);
+UserRouter.post("/login", loginUser);
+UserRouter.post("/change-pwd", changePassword);
+UserRouter.put("/update-user", updateUser);
+UserRouter.delete("/delete-user", deleteUser);
+
+export default UserRouter;
--- server/server.mjs
+++ server/server.mjs
@@ -1,10 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cors from "cors";
 import databaseConnection from "./config/database.mjs";
 import logger from "./utils/logger.mjs";
-
+import UserRouter from "./routes/UserRoute.mjs";
 
 const app = express();
 const PORT = process.env.PORT || "8080";
@@ -14,8 +14,7 @@ dotenv.config();
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true
-}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json({ limit: "20mb", extended: true }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
@@ -23,8 +22,9 @@ app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    logger.info(`Server is up and running on port ${PORT}`)
-    databaseConnection();
-})
+app.use("/user", UserRouter);
 
+app.listen(PORT, () => {
+  logger.info(`Server is up and running on port ${PORT}`);
+  databaseConnection();
+});
